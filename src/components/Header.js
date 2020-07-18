9561251460a3a2690b81c92dfad3b8ca2d7c00f3
kyrylo <bluejeansmonk@gmail.com>
2020-07-18 16:38:57 +0300

    accessability improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React from "react"
-import {Navbar, Nav} from "react-bootstrap"
-import {Link} from "gatsby"
+import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "gatsby"
 
-export default (props) => (
-    <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-                <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+export default props => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link as={Link} to="/">
+          <b className="text-dark">Home</b>
+        </Nav.Link>
+        <Nav.Link as={Link} to="/about">
+          <b className="text-dark">About Us</b>
+        </Nav.Link>
+        <Nav.Link as={Link} to="/contact">
+          <b className="text-dark">Contact Us</b>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
